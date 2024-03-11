dd12597c4d287c2c7933e24978551249aa65af79
kyoichi-sugahara <kyoichi.sugahara@tier4.jp>
2024-03-11 19:21:11 +0900

    add dummy cgmres interface
    Signed-off-by: kyoichi-sugahara <kyoichi.sugahara@tier4.jp>

--- control/mpc_lateral_controller/include/mpc_lateral_controller/mpc.hpp
+++ control/mpc_lateral_controller/include/mpc_lateral_controller/mpc.hpp
@@ -446,7 +446,7 @@ public:
   bool calculateMPC(
     const SteeringReport & current_steer, const Odometry & current_kinematics,
     AckermannLateralCommand & ctrl_cmd, Trajectory & predicted_trajectory,
-    Float32MultiArrayStamped & diagnostic);
+    Float32MultiArrayStamped & diagnostic, const std::string & qp_solver_type = "osqp");
 
   /**
    * @brief Set the reference trajectory to be followed.
