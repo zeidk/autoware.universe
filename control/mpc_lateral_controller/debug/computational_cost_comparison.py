ed34a24c00df3d10255e17e5092fc78e376c3fa9
kyoichi-sugahara <kyoichi.sugahara@tier4.jp>
2024-04-18 19:41:19 +0900

    temp
    Signed-off-by: kyoichi-sugahara <kyoichi.sugahara@tier4.jp>

--- control/mpc_lateral_controller/debug/computational_cost_comparison.py
+++ control/mpc_lateral_controller/debug/computational_cost_comparison.py
@@ -24,23 +24,33 @@ def extract_calculation_times(log_file_path):
     cgmres_pattern = (
         r"\[controller\.lateral_controller\]: executeOptimization \(cgmres\) time = (\d+) \[ns\]"
     )
+    generate_mpc_matrix_pattern = (
+        r"\[controller\.lateral_controller\]: generateMPCMatrix time = (\d+) \[ns\]"
+    )
 
     osqp_times = re.findall(osqp_pattern, log_data)
     cgmres_times = re.findall(cgmres_pattern, log_data)
+    generate_mpc_matrix_times = re.findall(generate_mpc_matrix_pattern, log_data)
 
     osqp_times = [int(time) / 1000000 for time in osqp_times]  # Convert nsec to msec
     cgmres_times = [int(time) / 1000000 for time in cgmres_times]  # Convert nsec to msec
+    generate_mpc_matrix_times = [
+        int(time) / 1000000 for time in generate_mpc_matrix_times
+    ]  # Convert nsec to msec
 
-    return osqp_times, cgmres_times
+    return osqp_times, cgmres_times, generate_mpc_matrix_times
 
 
-def plot_calculation_times(osqp_times, cgmres_times, log_file_name):
+def plot_calculation_times(osqp_times, cgmres_times, generate_mpc_matrix_times, log_file_name):
     plt.figure(figsize=(10, 6))
     plt.plot(range(len(osqp_times)), osqp_times, label="OSQP")
     plt.plot(range(len(cgmres_times)), cgmres_times, label="CGMRES")
+    plt.plot(
+        range(len(generate_mpc_matrix_times)), generate_mpc_matrix_times, label="generateMPCMatrix"
+    )
     plt.xlabel("Iteration")
     plt.ylabel("Calculation Time [ms]")  # Change the unit to msec
-    plt.title(f"OSQP vs CGMRES Calculation Time\nLog File: {log_file_name}")
+    plt.title(f"OSQP vs CGMRES vs generateMPCMatrix Calculation Time\nLog File: {log_file_name}")
     plt.legend()
     plt.grid(True)
     plt.show()
@@ -48,22 +58,21 @@ def plot_calculation_times(osqp_times, cgmres_times, log_file_name):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Plot OSQP and CGMRES calculation times from a log file."
+        description="Plot OSQP, CGMRES, and generateMPCMatrix calculation times from a log file."
     )
-    parser.add_argument("log_directory", help="Directory containing the log files.")
     parser.add_argument(
-        "-f",
-        "--file",
-        help="Specify the log file name. If not provided, the latest log file will be used.",
+        "log_path", help="Directory containing the log files or path to a specific log file."
     )
     args = parser.parse_args()
 
-    if args.file:
-        log_file_name = args.file
-        log_file_path = os.path.join(args.log_directory, log_file_name)
+    if os.path.isdir(args.log_path):
+        log_file_name = find_latest_log_file(args.log_path)
+        log_file_path = os.path.join(args.log_path, log_file_name)
+    elif os.path.isfile(args.log_path):
+        log_file_path = args.log_path
+        log_file_name = os.path.basename(args.log_path)
     else:
-        log_file_name = find_latest_log_file(args.log_directory)
-        log_file_path = os.path.join(args.log_directory, log_file_name)
+        raise ValueError("Invalid log path. Please provide a directory or a file path.")
 
-    osqp_times, cgmres_times = extract_calculation_times(log_file_path)
-    plot_calculation_times(osqp_times, cgmres_times, log_file_name)
+    osqp_times, cgmres_times, generate_mpc_matrix_times = extract_calculation_times(log_file_path)
+    plot_calculation_times(osqp_times, cgmres_times, generate_mpc_matrix_times, log_file_name)
