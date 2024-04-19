ce39b438f5505426e2317b775a698ea62578432b
kyoichi-sugahara <kyoichi.sugahara@tier4.jp>
2024-04-19 21:44:54 +0900

    temp
    Signed-off-by: kyoichi-sugahara <kyoichi.sugahara@tier4.jp>

--- control/mpc_lateral_controller/debug/visualize_path.py
+++ control/mpc_lateral_controller/debug/visualize_path.py
@@ -14,10 +14,33 @@ def read_csv(directory, filename):
     return [[float(x) for x in row if x] for row in data]
 
 
+def find_latest_directory(log_directory):
+    trajectory_directories = [
+        d
+        for d in os.listdir(log_directory)
+        if os.path.isdir(os.path.join(log_directory, d)) and d.startswith("trajectory_")
+    ]
+    if trajectory_directories:
+        return max(trajectory_directories)
+    return None
+
+
 parser = argparse.ArgumentParser(description="Plot trajectory data from CSV files.")
-parser.add_argument("directory", type=str, help="Directory containing the CSV files")
+parser.add_argument("--directory", type=str, help="Directory containing the CSV files")
 args = parser.parse_args()
-data_directory = args.directory
+
+home_directory = os.path.expanduser("~")
+log_directory = os.path.join(home_directory, ".ros", "log")
+
+if args.directory:
+    data_directory = args.directory
+else:
+    latest_directory = find_latest_directory(log_directory)
+    if latest_directory:
+        data_directory = os.path.join(log_directory, latest_directory)
+    else:
+        print("No trajectory directory found.")
+        exit(1)
 
 resampled_data = read_csv(data_directory, "resampled_x.log")
 predicted_data = read_csv(data_directory, "predicted_x.log")
@@ -39,7 +62,8 @@ plt.subplots_adjust(bottom=0.2)
 
 ax.set_xlabel("X")
 ax.set_ylabel("Y")
-ax.set_title("Trajectory Comparison")
+directory_name = os.path.basename(data_directory)
+ax.set_title(f"Trajectory Comparison\n{directory_name}")
 ax.legend()
 ax.grid(True)
 plt.gca().set_aspect("equal", adjustable="box")
@@ -74,7 +98,6 @@ def update(time_index):
 
 
 time_slider.on_changed(update)
-
 update(0)
 
 plt.show()
